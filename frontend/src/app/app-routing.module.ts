32d7412cc7fd996deb84a95a2696d67b8cd772e4
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-06-03 16:16:20 +0200

    feat: #16 Ajout de route au routeur

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {ListVehiculeComponent} from "./list-vehicule/list-vehicule.component";
+import {AccountComponent} from "./account/account.component";
 
 const routes: Routes = [
-  { path: "", component: HomeComponent }
+  { path: "", component: HomeComponent },
+  { path: "locations", component: ListVehiculeComponent},
+
+  { path: "mon-compte", component: AccountComponent}
 ];
 
 @NgModule({
