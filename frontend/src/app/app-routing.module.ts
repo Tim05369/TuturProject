711d923def1f0404f02dd3b6cf94a4ed8efee2f6
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-06-03 16:26:12 +0200

    feat: #16 Ajout de route contact et correction d'erreur

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ListVehiculeComponent} from "./list-vehicule/list-vehicule.component";
 import {AccountComponent} from "./account/account.component";
+import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "locations", component: ListVehiculeComponent},
-
+  { path: "contact", component: ContactComponent},
   { path: "mon-compte", component: AccountComponent}
 ];
 
