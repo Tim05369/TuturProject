d9dbee99a10747e3d8be320754a1a673ce75252b
gimeneti <Tim.Gimenez@etu.univ-grenoble-alpes.fr>
2021-06-03 15:59:34 +0200

    feat: #21 création du composant detailsVehicules

---
+++ frontend/src/app/details-vehicule/details-vehicule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-vehicule',
+  templateUrl: './details-vehicule.component.html',
+  styleUrls: ['./details-vehicule.component.scss']
+})
+export class DetailsVehiculeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
