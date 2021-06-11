602d159d5ff5a10ac9143374b0b04ef06c5b393d
gimeneti <Tim.Gimenez@etu.univ-grenoble-alpes.fr>
2021-06-11 13:15:48 +0200

    feat: #20 creation interface vehicule

---
+++ frontend/src/app/Vehicule.ts
@@ -0,0 +1,23 @@
+export interface Vehicule {
+    id: number;
+    brand: string;
+    model: string;
+    color: string;
+    horsePower : number;
+    rentalPrice: number;
+    kmPrice: number;
+    urlImg: string;
+    licencePlate: string
+
+    /*
+        "brand": "string",
+        "color": "string",
+        "horsePower": 0,
+        "id": 0,
+        "kmPrice": 0,
+        "licencePlate": "string",
+        "model": "string",
+        "rentalPrice": 0,
+        "urlImg": "string"
+     */
+}
