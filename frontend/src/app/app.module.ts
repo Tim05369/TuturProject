0915aef8c4ec20477bd95359574675bac2a95b26
gimeneti <Tim.Gimenez@etu.univ-grenoble-alpes.fr>
2021-06-13 16:43:04 +0200

    feat: #9 integration module httpClient & matCard

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactComponent } from './contact/contact.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {HttpClientModule} from "@angular/common/http";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {HttpClientModule} from "@angular/common/http";
     MatToolbarModule,
     MatButtonModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
