31d8bedd8879cbcb588a52b98f77d7f1bd12f4ac
gimeneti <Tim.Gimenez@etu.univ-grenoble-alpes.fr>
2021-06-14 20:34:51 +0200

    feat: #9 methode de service vehicule getVehiculeFilterByBrand()

--- frontend/src/app/vehicule.service.ts
+++ frontend/src/app/vehicule.service.ts
@@ -11,6 +11,7 @@ export class VehiculeService {
 
   private vehiculesURL = "http://localhost:8080/api/v1/vehicles"
   private vehiculesFilterByColorURL = "http://localhost:8080/api/v1/vehicles/color/"
+  private vehiculesFilterByBrandURL = "http://localhost:8080/api/v1/vehicles/brand/"
 
   constructor(private httpclient: HttpClient) { }
 
@@ -20,5 +21,9 @@ export class VehiculeService {
 
   getVehiculesFilterByColor(color: any) {
     return this.httpclient.get<Vehicule[]>(this.vehiculesFilterByColorURL+color);
+  }
+
+  getVehiculesFilterByBrand(brand: String) {
+    return this.httpclient.get<Vehicule[]>(this.vehiculesFilterByBrandURL+brand);
   }
 }
