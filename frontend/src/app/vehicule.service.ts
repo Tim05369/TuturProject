20df77225ad0754c79b8c48acf1f34633628c938
gimeneti <Tim.Gimenez@etu.univ-grenoble-alpes.fr>
2021-06-14 20:25:33 +0200

    feature: #9 methode getVehiculeFilterByColor() du service vehicule

--- frontend/src/app/vehicule.service.ts
+++ frontend/src/app/vehicule.service.ts
@@ -10,10 +10,15 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class VehiculeService {
 
   private vehiculesURL = "http://localhost:8080/api/v1/vehicles"
+  private vehiculesFilterByColorURL = "http://localhost:8080/api/v1/vehicles/color/"
 
   constructor(private httpclient: HttpClient) { }
 
   getVehicules(): Observable<Vehicule[]>{
     return this.httpclient.get<Vehicule[]>(this.vehiculesURL);
+  }
+
+  getVehiculesFilterByColor(color: any) {
+    return this.httpclient.get<Vehicule[]>(this.vehiculesFilterByColorURL+color);
   }
 }
