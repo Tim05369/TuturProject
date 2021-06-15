7681da533f75e261cdb8fec927f5bd37ac577358
gimeneti <Tim.Gimenez@etu.univ-grenoble-alpes.fr>
2021-06-15 19:10:23 +0200

    feat: #38 definition de la route admin

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {ListVehiculeComponent} from "./list-vehicule/list-vehicule.component";
 import {AccountComponent} from "./account/account.component";
 import {ContactComponent} from "./contact/contact.component";
 import {DetailsVehiculeComponent} from "./details-vehicule/details-vehicule.component";
+import {AdminComponent} from "./admin/admin.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "locations", component: ListVehiculeComponent},
   { path: "contact", component: ContactComponent},
   { path: "mon-compte", component: AccountComponent},
-  { path: "locations/vehicule", component: DetailsVehiculeComponent}
+  { path: "locations/vehicule", component: DetailsVehiculeComponent},
+  { path: "admin", component: AdminComponent}
 ];
 
 @NgModule({
