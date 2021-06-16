95f649c2f803df098507876ed6eb2560d171f33b
gimeneti <Tim.Gimenez@etu.univ-grenoble-alpes.fr>
2021-06-16 15:23:42 +0200

    feat: #39 ajout de methode permettant de savoir si un objet vehicule contient toutes les clés

--- frontend/src/app/Vehicule.ts
+++ frontend/src/app/Vehicule.ts
@@ -9,3 +9,14 @@ export interface Vehicule {
     urlImg: string;
     licencePlate: string
 }
+
+export function vehiculeContainsAllKey(vehicule: Vehicule) {
+    return "brand" in vehicule &&
+        "color" in vehicule &&
+        "horsePower" in vehicule &&
+        "kmPrice" in vehicule &&
+        "licencePlate" in vehicule &&
+        "model" in vehicule &&
+        "rentalPrice" in vehicule &&
+        "urlImg" in vehicule;
+}
